9d2fb12aa283f68b6f311f9510a9a37824667dcf
rajgiri8 <freedomcomets@berkeley.edu>
2019-10-16 19:30:33 -0700

    part1 completed

--- rottenpotatoes/features/sort_movie_list.feature
+++ rottenpotatoes/features/sort_movie_list.feature
@@ -24,7 +24,7 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
-  # your steps here
+  #
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
+  
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+   #page.body.should =~ /e2.*e2
+   fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
